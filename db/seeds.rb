2255b4c213fd49ff4b8fccabe81a2ad70b9bc91a
vveleva <vvveleva@gmail.com>
2015-02-03 17:49:26 -0800

    Add more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,7 @@ place = provider.places.create(title: "Starbucks",
         longitude: 37.783921,
         latitude: -122.4073459)
 provider2 = User.create(email: "provider2", password: "awesome", provider: true)
-place = provider2.places.create(title: "Workshop Cafe",
+place2 = provider2.places.create(title: "Workshop Cafe",
         street_address: "180 Montgomery Street #100",
         city: "San Francisco",
         state: "CA",
@@ -26,7 +26,7 @@ place = provider2.places.create(title: "Workshop Cafe",
         longitude: 37.790815,
         latitude: -122.402144)
 provider3 = User.create(email: "provider3", password: "awesome", provider: true)
-place = provider3.places.create(title: "Tu Lan Vietnamese Restaurant",
+place3 = provider3.places.create(title: "Tu Lan Vietnamese Restaurant",
         street_address: "8 6th Street",
         city: "San Francisco",
         state: "CA",
@@ -34,3 +34,16 @@ place = provider3.places.create(title: "Tu Lan Vietnamese Restaurant",
         country: "United States",
         longitude: 37.7818066,
         latitude: -122.4104021)
+
+workshop_menu = place2.menus.create(title: "Workshop Cafe's Menu")
+workshop_drinks = workshop_menu.categories.create(title: "Drinks")
+workshop_lunch = workshop_menu.categories.create(title: "Lunch")
+workshop_dinner = workshop_menu.categories.create(title: "Dinner")
+
+workshop_drinks.menu_items.create(title: "Latte", image_url: nil)
+workshop_drinks.menu_items.create(title: "Cappucchino", image_url: nil)
+workshop_drinks.menu_items.create(title: "Black Coffee", image_url: nil)
+workshop_drinks.menu_items.create(title: "Caramel Latte", image_url: nil)
+workshop_drinks.menu_items.create(title: "Tea", image_url: nil)
+workshop_lunch.menu_items.create(title: "Club Sandwich", image_url: nil)
+workshop_lunch.menu_items.create(title: "BLT Salad", image_url: nil)
